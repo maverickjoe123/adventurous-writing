26bdfaea516c468dcb9ad68b83c42120cac2e178
maverickjoe123 <55029042+maverickjoe123@users.noreply.github.com>
2020-05-24 18:28:13 -0400

    adding a Mango

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
 INSERT INTO known_fruits(id, name, stock) VALUES (1, 'Apple', 10);
 INSERT INTO known_fruits(id, name, stock) VALUES (2, 'Orange', 10);
 INSERT INTO known_fruits(id, name, stock) VALUES (3, 'Pear', 10);
+INSERT INTO known_fruits(id, name, stock) VALUES (4, 'Mango', 10);
